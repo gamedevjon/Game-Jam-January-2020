0ebb63652d488455dc7191ee9e4cb589a92b96ab
jmacnut <jmacnut@gmail.com>
2020-01-04 15:48:45 -1000

    adds ti leaf catcher placeholder with movement

--- Iholena/Assets/Scripts/TiLeaf.cs
+++ Iholena/Assets/Scripts/TiLeaf.cs
@@ -6,6 +6,8 @@ public class TiLeaf : MonoBehaviour
 {
    [SerializeField]
    private float _speed;
+   private float _rightLimit = 9.6f;
+   private float _leftLimit = -9.66f;
 
    // Start is called before the first frame update
    void Start()
@@ -16,15 +18,28 @@ public class TiLeaf : MonoBehaviour
    // Update is called once per frame
    void Update()
    {
-      if (Input.GetKeyDown(KeyCode.RightArrow))
+      float horizontalPos;
+      float verticalPos = -4.5f;
+      Vector3 newPosition;
+
+      if (Input.GetKey(KeyCode.RightArrow))
       {
+         if (transform.position.x >= _rightLimit)
+         {
+            newPosition = new Vector3(_leftLimit, verticalPos, 0);
+            transform.position = newPosition;
+         }
          transform.Translate(Vector3.right * _speed * Time.deltaTime);
       }
 
-      if (Input.GetKeyDown(KeyCode.LeftArrow))
+      if (Input.GetKey(KeyCode.LeftArrow))
       {
+         if (transform.position.x <= _leftLimit)
+         {
+            newPosition = new Vector3(_rightLimit, verticalPos, 0);
+            transform.position = newPosition;
+         }
          transform.Translate(Vector3.left * _speed * Time.deltaTime);
-
       }
    }
 }
