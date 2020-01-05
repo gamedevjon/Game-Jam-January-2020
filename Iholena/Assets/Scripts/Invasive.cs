5026868bdcfaf9733b5a3ebefdb96e1bb2c096da
Paul Keahi <paulkeahi@pauls-mbp.ksbe.edu>
2020-01-04 15:44:21 -1000

    Base Scene Implementation

---
+++ Iholena/Assets/Scripts/Invasive.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invasive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == null)
+        {
+            Debug.Log("Error, Invasive Object is null");
+        }
+
+        if (collision.tag == "Player")
+        {
+            //Damage Player
+            Debug.Log("Player Damage Here");
+        }
+    }
+}
